de960139ce649f3365339267cff70a0fe96bf18a
Felipe Didio <felipe.sdidio@gmail.com>
2023-11-14 13:22:50 -0300

    fix: Replaced function next_task_id() function calls with task_queue.next_id() calls

--- client/src/network.cpp
+++ client/src/network.cpp
@@ -1,10 +1,12 @@
 #include <cstdint>
+#include <optional>
 #include <pthread.h>
 #include <queue>
 #include <string>
 #include <vector>
 #include <semaphore.h>
 
+#include "../include/task_queue.h"
 #include "../include/network.h"
 
 namespace Network {
@@ -13,64 +15,7 @@ namespace Network {
     std::string port;
 
     namespace __internal {
-        /*
-         * Queue
-         */
-        std::queue<network_task> task_queue;
-        std::queue<network_task> done_queue;
-
-        /*
-         * Semaphores for the management of tasks.
-         * - mutex: makes sure only one thread is using the task queue
-         * - done_mutex: makes sure only one thread is using the done queue
-         * - available: counts how many tasks are available to be done
-         * - done: counts how many tasks are done.
-         */
-        sem_t mutex, done_mutex, available, done;
-
-        /*
-         * Generates a unique task ID.
-         */
-        int LAST_TASK_ID = 0;
-        int next_task_id() {
-            return __internal::LAST_TASK_ID++;
-        }
-
-        /*
-         * Takes a task from que task queue and puts it into the 'task' parameter.
-         * If the queue is empty, the thread is locked until a task is inserted.
-         */
-        void next_task(network_task *task) {
-            // Wait until there is a task on the queue
-            sem_wait(&available);
-            // Wait until the queue can be safely accessed
-            sem_wait(&mutex);
-
-            // Get next task from queue
-            network_task next = task_queue.front();
-            task_queue.pop();
-            
-            // Release queue
-            sem_post(&mutex);
-
-            // Copy next's data onto task
-            task->type = next.type;
-            task->task_id = next.task_id;
-            task->username = next.username;
-            task->filename = next.filename;
-            task->content = next.content;
-            task->files = next.files;
-        }
-
-        /*
-         * Adds the task to the done queue
-         */
-        void finish_task(network_task *task) {
-            sem_wait(&done_mutex);
-            done_queue.push(*task);
-            sem_post(&done_mutex);
-            sem_post(&done);
-        }
+        TaskQueue::async_task_queue<Network::network_task> task_queue;
 
         /*
          * The networking thread function
@@ -79,21 +24,22 @@ namespace Network {
             network_task task;
 
             while (1) {
-                next_task(&task);
+                task = task_queue.take();
+                
                 // TODO - Didio: Process the task
                 switch (task.type) {
+                    case TASK_LIST_FILES:
+                        break;
                     case TASK_UPLOAD:
                         break;
                     case TASK_DOWNLOAD:
                         break;
                     case TASK_DELETE:
                         break;
-                    case TASK_LIST_FILES:
-                        break;
                     case TASK_EXIT:
                         break;
                 }
-                finish_task(&task);
+                task_queue.insert_done(task);
             }
 
             pthread_exit(nullptr);
@@ -107,11 +53,8 @@ namespace Network {
         Network::ip = ip;
         Network::port = port;
 
-        sem_init(&__internal::mutex, 0, 1);
-        sem_init(&__internal::done_mutex, 0, 1);
-        sem_init(&__internal::available, 0, 0);
-        sem_init(&__internal::done, 0, 0);
-         
+        __internal::task_queue = TaskQueue::async_task_queue<network_task>();
+
         pthread_create(&network_thread, NULL, __internal::thread_function, nullptr);
     }
 
@@ -119,10 +62,8 @@ namespace Network {
      * Adds an upload task to the task queue and returns the task id
      */
     int upload_file(std::string username, std::string path) {
-        sem_wait(&__internal::mutex);
-        int task_id = __internal::next_task_id();
-        __internal::task_queue.push({
-
+        int task_id = __internal::task_queue.next_id();
+        __internal::task_queue.insert({
                 TASK_UPLOAD, 
                 task_id, 
                 username,
@@ -130,8 +71,6 @@ namespace Network {
                 nullptr,
                 std::vector<FileManager::file_description>(),
                 });
-        sem_post(&__internal::mutex);
-        sem_post(&__internal::available);
         return task_id;
     }
 
@@ -139,9 +78,8 @@ namespace Network {
      * Adds a download task to the task queue and returns the task id
      */
     int download_file(std::string username, std::string path) {
-        sem_wait(&__internal::mutex);
-        int task_id = __internal::next_task_id();
-        __internal::task_queue.push({
+        int task_id = __internal::task_queue.next_id();
+        __internal::task_queue.insert({
                 TASK_DOWNLOAD, 
                 task_id, 
                 username,
@@ -149,8 +87,6 @@ namespace Network {
                 nullptr,
                 std::vector<FileManager::file_description>(),
                 });
-        sem_post(&__internal::mutex);
-        sem_post(&__internal::available);
         return task_id;
     }
 
@@ -158,9 +94,8 @@ namespace Network {
      * Adds a delete task to the task queue and returns the task id
      */
     int delete_file(std::string username, std::string path) {
-        sem_wait(&__internal::mutex);
-        int task_id = __internal::next_task_id();
-        __internal::task_queue.push({
+        int task_id = __internal::task_queue.next_id();
+        __internal::task_queue.insert({
                 TASK_DELETE, 
                 task_id, 
                 username,
@@ -168,8 +103,6 @@ namespace Network {
                 nullptr,
                 std::vector<FileManager::file_description>(),
                 });
-        sem_post(&__internal::mutex);
-        sem_post(&__internal::available);
         return task_id;
     }
 
@@ -177,9 +110,8 @@ namespace Network {
      * Adds a exit task to the task queue and locks the thread until the networking subsystem is properly shutdown
      */
     int client_exit(std::string username) {
-        sem_wait(&__internal::mutex);
-        int task_id = __internal::next_task_id();
-        __internal::task_queue.push({
+        int task_id = __internal::task_queue.next_id();
+        __internal::task_queue.insert({
                 TASK_EXIT, 
                 task_id, 
                 username,
@@ -187,8 +119,6 @@ namespace Network {
                 nullptr,
                 std::vector<FileManager::file_description>(),
                 });
-        sem_post(&__internal::mutex);
-        sem_post(&__internal::available);
         return task_id;
     }
 
@@ -196,9 +126,8 @@ namespace Network {
      * Adds a list files task to the task queue and returns the task id
      */
     int list_files(std::string username, std::string path) {
-        sem_wait(&__internal::mutex);
-        int task_id = __internal::next_task_id();
-        __internal::task_queue.push({
+        int task_id = __internal::task_queue.next_id();
+        __internal::task_queue.insert({
                 TASK_LIST_FILES, 
                 task_id, 
                 username,
@@ -206,8 +135,6 @@ namespace Network {
                 nullptr,
                 std::vector<FileManager::file_description>(),
                 });
-        sem_post(&__internal::mutex);
-        sem_post(&__internal::available);
         return task_id;
     }
 
@@ -216,28 +143,15 @@ namespace Network {
      * Returns true, if there is a task in the done queue. Returns false otherwise.
      */
     bool try_get_task(network_task *task) {
-        // Try to acquire the `done` semaphore
-        // (meaning that there is a task in the done queue)
-        if (sem_trywait(&__internal::done) == 0) {
-            // If it can acquire the `done`, it locks the `done_mutex` lock
-            sem_wait(&__internal::done_mutex);
-
-            // Pop an item out of the done_queue
-            network_task done_task = __internal::done_queue.front();
-            __internal::done_queue.pop();
-            
-            // Release the `done_mutex` lock
-            sem_post(&__internal::done_mutex);
-
-            // Copy the data from the done task to task
+        std::optional<network_task> _done_task = __internal::task_queue.try_take_done();
+        if (_done_task) {
+            network_task done_task = _done_task.value();
             task->type = done_task.type;
             task->task_id = done_task.task_id;
             task->username = done_task.username;
             task->filename = done_task.filename;
             task->content = done_task.content;
             task->files = done_task.files;
-
-            // Signal that the opeartion of successful
             return true;
         } else {
             return false;
@@ -249,17 +163,7 @@ namespace Network {
      * Immediately returns, if there is a task in the done queue, waits otherwise.
      */
     void get_task(network_task *task) {
-        // Wait until there is a task in the done queue
-        sem_wait(&__internal::done);
-        // If it can acquire the `done`, it locks the `done_mutex` lock
-        sem_wait(&__internal::done_mutex);
-
-        // Pop an item out of the done_queue
-        network_task done_task = __internal::done_queue.front();
-        __internal::done_queue.pop();
-
-        // Release the `done_mutex` lock
-        sem_post(&__internal::done_mutex);
+        network_task done_task = __internal::task_queue.take_done();
 
         // Copy the data from the done task to task
         task->type = done_task.type;
