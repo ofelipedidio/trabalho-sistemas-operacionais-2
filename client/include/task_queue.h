a4643a90a48bb5ec8fd798d896ae2af3f7640e33
Felipe Didio <felipe.sdidio@gmail.com>
2023-11-15 19:02:41 -0300

    feat: Implemented a one-way synchronized task queue for asynchronous communication amongst threads

--- client/include/task_queue.h
+++ client/include/task_queue.h
@@ -2,64 +2,44 @@
 #include <semaphore.h>
 #include <optional>
 
-namespace TaskQueue {
-    template <typename task_t>
-        class async_task_queue {
+namespace AsyncQueue {
+    template <typename item_t>
+        class async_queue {
             private:
-                std::queue<task_t> in_queue;
-                std::queue<task_t> out_queue;
-                sem_t in_mutex, out_mutex, in_ready, out_ready;
+                std::queue<item_t> queue;
+                sem_t mutex, awaiting;
                 int last_id;
 
             public:
-                async_task_queue() {
+                async_queue() {
                     this->last_id = 0;
-                    this->in_queue = std::queue<task_t>();
-                    this->out_queue = std::queue<task_t>();
-                    sem_init(&this->in_mutex, 0, 1);
-                    sem_init(&this->out_mutex, 0, 1);
-                    sem_init(&this->in_ready, 0, 0);
-                    sem_init(&this->out_ready, 0, 0);
+                    this->queue = std::queue<item_t>();
+                    sem_init(&this->mutex, 0, 1);
+                    sem_init(&this->awaiting, 0, 0);
                 }
 
-                void insert(task_t task) {
-                    sem_wait(&this->in_mutex);
-                    this->in_queue.push(task);
-                    sem_post(&this->in_mutex);
-                    sem_post(&this->in_ready);
+                void push(item_t task) {
+                    sem_wait(&this->mutex);
+                    this->queue.push(task);
+                    sem_post(&this->mutex);
+                    sem_post(&this->awaiting);
                 }
 
-                task_t take() {
-                    sem_wait(&this->in_ready);
-                    sem_wait(&this->in_mutex);
-                    task_t task = this->in_queue.front();
-                    this->in_queue.pop();
-                    sem_post(&this->in_mutex);
+                item_t pop() {
+                    sem_wait(&this->awaiting);
+                    sem_wait(&this->mutex);
+                    item_t task = this->queue.front();
+                    this->queue.pop();
+                    sem_post(&this->mutex);
                     return task;
                 }
 
-                void insert_done(task_t task) {
-                    sem_wait(&this->out_mutex);
-                    this->out_queue.push(task);
-                    sem_post(&this->out_mutex);
-                    sem_post(&this->out_ready);
-                }
-
-                task_t take_done() {
-                    sem_wait(&this->out_ready);
-                    sem_wait(&this->out_mutex);
-                    task_t task = this->out_queue.front();
-                    this->out_queue.pop();
-                    sem_post(&this->out_mutex);
-                    return task;
-                }
-
-                std::optional<task_t> try_take_done() {
-                    if (sem_trywait(&this->out_ready)) {
-                        sem_wait(&this->out_mutex);
-                        task_t task = this->out_queue.front();
-                        this->out_queue.pop();
-                        sem_post(&this->out_mutex);
+                std::optional<item_t> try_pop() {
+                    if(sem_trywait(&this->awaiting) == 0) {
+                        sem_wait(&this->mutex);
+                        item_t task = this->queue.front();
+                        this->queue.pop();
+                        sem_post(&this->mutex);
                         return task;
                     } else {
                         return {};
@@ -67,9 +47,9 @@ namespace TaskQueue {
                 }
 
                 int next_id() {
-                    sem_wait(&this->in_mutex);
+                    sem_wait(&this->mutex);
                     return this->last_id++;
-                    sem_post(&this->in_mutex);
+                    sem_post(&this->mutex);
                 }
         };
 }
