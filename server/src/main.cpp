e9a6284a7bfcbf998d2c3d168ed4182a661c19b1
Felipe Didio <felipe.sdidio@gmail.com>
2023-11-16 19:21:35 -0300

    feat: [server] Created a tcp hexdump to debug the application

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,5 +1,17 @@
 // Configurar sistemas
+#include "../include/network.h"
+#include <cstdint>
+#include <sstream>
+#include <string>
 
-int main() {
+int main(int argc, char** argv) {
+    uint16_t port = 54321;
+    if (argc >= 2) {
+        std::string s(argv[1]);
+        std::istringstream iss(s);
+        iss >> port;
+    }
+
+    tcp_dump("0.0.0.0", port);
     return 0;
 }
