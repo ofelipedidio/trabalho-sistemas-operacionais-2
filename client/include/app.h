1fbf00b2657203fba114d47d499a703f6c5dc695
Felipe Didio <felipe.sdidio@gmail.com>
2023-12-02 13:25:31 -0300

    chore: [Client] Removed task queue from app

--- client/include/app.h
+++ client/include/app.h
@@ -8,42 +8,17 @@
 #include "async_queue.h"
 
 namespace App {
-
-    enum task_type {
-        TASK_LIST_SERVER = 1,
-        TASK_UPLOAD = 2,
-        TASK_DOWNLOAD = 3,
-        TASK_DELETE = 4,
-        TASK_GET_SYNC = 5
-    };
-
-    typedef struct app_task {
-        int type;
-        int task_id;
-        std::string username;
-        std::string filename;
-        uint8_t *content;
-        std::vector<FileManager::file_description> files;
-
-    } app_task;
-
-    AsyncQueue::async_queue<app_task> task_queue;
-    sem_t sync_cli;
-
     void init(std::string username);
     void notify_new_file(std::string filename);
     void notify_modified(std::string filename);
     void notify_deleted(std::string filename);
     
+    void task_done(uint8_t* mem);
 
-    void taks_done(uint8_t* mem);
-
-    std::vector<file_description> list_server();
+    std::vector<FileManager::file_description> list_server();
     void upload_file(std::string path);
     void download_file(std::string filename);
     void delete_file(std::string path);
     void get_sync_dir(std::string username);
-
-
 }
 
