6bac36070490ce84aa02fc604d0fca1c21b84e1d
Felipe Didio <felipe.sdidio@gmail.com>
2023-12-04 11:17:52 -0300

    new run.sh

--- server/run.sh
+++ server/run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-clear
-make compile
-cd bin
-./server $1
+while true; do
+    clear
+    make compile
+    cd bin
+    ./server $1
+    for pid in $(lsof -i tcp:4000 | awk 'NR!=1 {print $2}')
+    do
+        kill -9 $pid
+    done
+    sleep 2
+done
