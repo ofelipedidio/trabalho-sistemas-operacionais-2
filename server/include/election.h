21acd1cb5f135794c03cddf3338f8d125e4b55bc
joaoPauloDias <joaopaulovasquezdias@gmail.com>
2024-01-26 17:52:12 -0300

    header file election functions

--- server/include/election.h
+++ server/include/election.h
@@ -17,3 +17,15 @@ typedef struct {
 typedef struct {
     std::vector<server_t> servers;
 } metadata_t;
+
+void initiateElection(metadata_t& metadata);
+
+void receiveElectionMessage(metadata_t& metadata, const server_t& senderServer);
+
+void receiveElectedMessage(metadata_t& metadata, const server_t& electedServer);
+
+server_t getNextServer(const metadata_t& metadata, const server_t& currentServer);
+
+void sendElectionMessage(const server_t& targetServer, const server_t& winningServer);
+
+void sendElectedMessage(const server_t& targetServer, const server_t& electedServer);
